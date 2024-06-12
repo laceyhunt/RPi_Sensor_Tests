e4563d41ae31d1737e0b0f147e9ac181e674a911
lacey <hunt3247@vandals.uidaho.edu>
2024-06-12 15:06:17 -0700

    changing init

--- single_axis.py
+++ single_axis.py
@@ -47,6 +47,8 @@ def read_sensor_data():
 # Sequence of writes to initialize the sensor
 write_i2c_block(0x0A, [0, 0])
 time.sleep(0.1)
+while GPIO.input(input_pin)!=GPIO.LOW:
+    pass
 
 data=read_i2c_block(0x02, 2)
 print(data)
