ba176d82b68eda29b77efee5dbb2c7860cd5d5dc
lacey <hunt3247@vandals.uidaho.edu>
2024-07-12 15:43:56 -0700

    editing new test

--- bend_sensor_test.py
+++ bend_sensor_test.py
@@ -68,11 +68,11 @@ def read_sensor_data():
     if data is not None:
         decoded_value1 = ads_int16_decode_single(data)
         if(DEVICE_ADDRESS==0x12):
-            print(f"Raw: {data}     Decoded: {decoded_value1}")
+            print(f"Decoded: {decoded_value1}      Raw: {data}")
         if(DEVICE_ADDRESS==0x13):
             decoded_value2 = ads_int16_decode_two(data)
             # print(f"Decoded: {decoded_value2}")
-            print(f"Raw: {data}     Decoded: {decoded_value1}, {decoded_value2}")
+            print(f"Decoded: {decoded_value1}, {decoded_value2}      Raw: {data}")
         return decoded_value1
     else:
         return None
